d29a7231edae1b0ba045865bab97683721243fc0
Clifford Tien <ts02690140@gmail.com>
2023-03-14 11:51:30 +0800

    feat(routes and path): setting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from 'COMPONENTS/navigation/navigation.component';
+import Home from 'ROUTES/home/home.component';
+
 import './App.css';
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<>
+			<Navigation />
+			<Routes>
+				<Route index element={<Home />} />
+			</Routes>
+		</>
 	);
 }
 
